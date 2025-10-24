e786a165729bc975154f309e6ac176edcb7f769a
Antoine <antoine.maurens@edu.ece.fr>
2025-10-24 14:37:02 +0200

    chore ; add gitignore

---
+++ rectangle.h
@@ -0,0 +1,13 @@
+//
+// Created by antoi on 24/10/2025.
+//
+
+#ifndef TP_FONCTIONS_RECTANGLE_H
+#define TP_FONCTIONS_RECTANGLE_H
+
+
+void afficherRectangle(int hauteur, int largeur);
+void Rectangle(void);
+
+
+#endif //TP_FONCTIONS_RECTANGLE_H
